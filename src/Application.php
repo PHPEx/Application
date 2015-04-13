a2d81216c6657e29a06642fe3b0a7f072368054e
IgorDePaula <principe.borodin@gmail.com>
2015-04-13 11:35:28 -0300

    Removed unnused statements

--- src/Application.php
+++ src/Application.php
@@ -2,38 +2,35 @@
 
 namespace Application;
 
-use Mvc\MvcInterface;
 use Mvc\AbstractController;
 use ArrayObject;
-use ReflectionClass;
 
-class Application
-{
+class Application {
 
-	private $controllers = array();
+    private $controllers = array();
 
-	public function __construct(){
-		$this->controllers = new ArrayObject();
-	}
+    public function __construct() {
+        $this->controllers = new ArrayObject();
+    }
 
-	public function registerController(AbstractController $controller){
-		$this->controllers->append($controller);
-	}	
+    public function registerController(AbstractController $controller) {
+        $this->controllers->append($controller);
+    }
 
-	public function run(){		
-		$iterator = $this->controllers->getIterator();
-		while($iterator->valid()){
-			$controller = $iterator->current();
-			$inspector = $controller->getInspector($controller);
-			$parse = $inspector->parse()->getParse();
-			array_map(array($this,'map'),(array)$parse);
-			$iterator->next();
-		}
-	}
+    public function run() {
+        $iterator = $this->controllers->getIterator();
+        while ($iterator->valid()) {
+            $controller = $iterator->current();
+            $inspector = $controller->getInspector($controller);
+            $parse = $inspector->parse()->getParse();
+            array_map(array($this, 'map'), (array) $parse);
+            $iterator->next();
+        }
+    }
 
-	private function map($prop){	
-		list($class,$method) = explode(':',$prop['action']);
- 		$object = new $class();
- 		$object->validate($prop['Inspector\Controller\Route'],$method);
-	}
+    private function map($prop) {
+        list($class, $method) = explode(':', $prop['action']);
+        $object = new $class();
+        $object->validate($prop['Inspector\Controller\Route'], $method);
+    }
 }
